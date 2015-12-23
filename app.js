87fc746b853a7dc1f50e94d1590880d301ff25fb
Karl Balemi <karl.balemi@auspost.com.au>
2015-12-23 17:35:18 +1100

    Survey.

--- app.js
+++ app.js
@@ -7,7 +7,46 @@ angular.module('mood-survey', ['ngRoute', 'firebase']
       .when('/login', {
         templateUrl: 'views/login.html',
         controller: 'LoginController'
+      }).when('/survey', {
+        templateUrl: 'views/survey.html',
+        controller: 'SurveyController'
       });
+
+  }).controller('SurveyController', function($scope, $routeParams, $firebaseObject, $timeout) {
+
+    var s = $routeParams.s;
+    var ref = new Firebase('https://amber-heat-8057.firebaseio.com/surveys/' + s);
+    var survey= $firebaseObject(ref);
+
+    $scope.moods = [
+      {val : 'veryBad', emoji : '?'},
+      {val : 'bad', emoji : '?'},
+      {val : 'fine', emoji : '?'},
+      {val : 'happy', emoji : '?'},
+      {val : 'veryHappy', emoji : '?'}
+    ];
+
+    survey.$loaded(function(response) {
+      $scope.show = true;
+      survey.$bindTo($scope, 'survey');
+      if(response.$value === null) {
+        //TODO: Show some kind of 404 page
+      }
+
+    });
+
+    $scope.animate = {};
+
+    $scope.addMood = function(mood) {
+      if(!$scope.animate[mood]) {
+
+        $scope.survey[mood] += 1;
+        $scope.animate[mood] = true;
+        $timeout(function(){
+          $scope.animate[mood] = false;
+        },1000);
+      }
+    };
 
   }).controller('LoginController', function ($scope, $firebaseAuth) {
 
